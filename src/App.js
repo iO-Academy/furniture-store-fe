aecf2fc4f68a5bedb5a853be04ca3b4abf3bdb5b
Mike Oram <mike.oram@mayden.co.uk>
2022-07-06 14:46:24 +0100

    make responsive

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import Home from "./Pages/Home";
 
 export default function App() {
     return (
-        <>
+        <BrowserRouter>
             <div className="container-fluid">
                 <div className="row bg-dark text-light">
                     <div className="col-12">
-                        <h1>Furniture Store</h1>
+                        <h1>
+                            <Link to={"/"} className="text-light">Furniture Store</Link>
+                        </h1>
                     </div>
                 </div>
             </div>
             <div className="container">
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                    </Routes>
-                </BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
                 <footer className="border-top mt-5 py-3">
                     <p>&copy; Copyright iO Academy 2022</p>
                 </footer>
             </div>
-        </>
+        </BrowserRouter>
     )
 }
--- src/Organisms/Categories/index.js
+++ src/Organisms/Categories/index.js
@@ -1,8 +1,6 @@
-import { Link } from "react-router-dom";
-
 export default function Category(props) {
     return (
-        <div className="col-3 mb-4">
+        <div className="col-12 col-sm-6 col-lg-3 mb-4">
             <div className="bg-light rounded p-3 mb-3">
                 <h3>{props.name}
                     <span className="badge badge-info float-right">{props.products}</span>
